2e79f78c22db05833f96a2ea7e398cab7aa93dc5
Reethi20 <reethibkumar@gmail.com>
2025-03-12 01:01:44 +0530

    Third commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use("/assets", express.static(path.resolve('assets')));
